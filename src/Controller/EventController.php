6fb7b762599623f719a665a8dfdb35599875b111
enure <enureshkigal@gmail.com>
2024-11-05 16:46:32 +0100

    add custom forms

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Form\EventFormType;
 use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -35,8 +37,8 @@ class EventController extends AbstractController
     /**
      * @throws \DateMalformedStringException
      */
-    #[Route('/{name}/{start}/{end}', name: 'app_event_new')]
-    public function newEvent
+    #[Route('/{name}/{start}/{end}', name: 'app_event_new_with_start_and_end')]
+    public function newEventWithStartAndEnd
     (
         string                 $name,
         string                 $start,
@@ -66,5 +68,21 @@ class EventController extends AbstractController
     {
         $event = $eventRepository->getEventsByDate(new \DateTimeImmutable($start), new \DateTimeImmutable($end));
         return new JsonResponse($event);
+    }
+
+    #[Route('/event/new', name: 'app_event_new', methods: ['GET', 'POST'])]
+    public function newEvent(Request $request, EntityManagerInterface $manager): Response
+    {
+        $event = new Event();
+        $form = $this->createForm(EventFormType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($event);
+            $manager->flush();
+            return $this->redirectToRoute('event_routeapp_event_query_by_id', ['id' => $event->getId()]);
+        }
+        return $this->render('event/new_event.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
