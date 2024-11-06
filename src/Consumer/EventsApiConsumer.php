c4fe060195ccb19357efaee24938ba03f6720916
Minh Hoang Tran <hoang.mht18@gmail.com>
2024-11-06 15:38:04 +0100

    - add events client for API request

--- src/Consumer/EventsApiConsumer.php
+++ src/Consumer/EventsApiConsumer.php
@@ -5,21 +5,35 @@ namespace App\Consumer;
 use App\Search\EventSearchInterface;
 use SensitiveParameter;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 readonly class EventsApiConsumer implements EventSearchInterface
 {
     public function __construct
     (
-        #[SensitiveParameter]
-        #[Autowire(env: 'EVENTS_API_KEY')]
-        private string $apiKey
+        private HttpClientInterface $eventsClient
     )
     {
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     public function searchByName(?string $name = null): array
     {
-        $this->apiKey;
-        return [];
+        // The third parameter of the `request` method is an array of options.
+        return $this->eventsClient->request('GET', '/events', [
+            'query' => ['name' => $name]
+        ])->toArray();
     }
 }
